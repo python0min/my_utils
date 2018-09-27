8866455e28cf658d3d43382a74105f891d46e20f
python0min <pythoner_min@163.com>
2018-09-27 15:20:02 +0800

    Initial commit

---
+++ django_utils.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Datetime : 18-9-20 下午6:02
+# @Author : min
+# @Site : 
+# @function : 平时经常网上搜索的功能,但由于经常会忘,所以还是记录下来,写在py文件
+
+
+def get_model_field():
+    """
+    获取model的verbose_name和name的数据库的字段名
+    # from mysite.manage.utils.import_util import get_model_field
+    """
+    from django.apps import apps
+    model_obj = apps.get_model('manage', 'monitor')  # 根据app_name,model_name获取对象
+    fileds = model_obj._meta.fields
+
+    for i in fileds:
+        print i.get_attname_column()[0]  # 数据库字段名
+        print i.name  # field的名称
+        print i.verbose_name  # 对应的中文名称
+
+
+if __name__ == '__main__':
+    pass
+
---
+++ python_utils.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Datetime : 18-9-20 下午6:02
+# @Author : min
+# @Site : 
+# @function : 平时经常网上搜索的功能,但由于经常会忘,所以还是记录下来,写在py文件
+
+
+def util1():
+    """
+    一.python的set和其他语言类似, 是一个无序不重复元素集, 基本功能包括关系测试和消除重复元素.
+    集合对象还支持union(联合), intersection(交), difference(差)和sysmmetric difference(对称差集)
+    等数学运算.
+    """
+
+
+if __name__ == '__main__':
+    pass
+
