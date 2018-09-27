fff68a4be566e70d425b48dd5ae50eabf8f91669
python0min <pythoner_min@163.com>
2018-09-27 17:33:08 +0800

    1.增加util2方法,判断对象类型

--- python_utils.py
+++ python_utils.py
@@ -9,11 +9,25 @@
 def util1():
     """
     一.python的set和其他语言类似, 是一个无序不重复元素集, 基本功能包括关系测试和消除重复元素.
-    集合对象还支持union(联合), intersection(交), difference(差)和sysmmetric difference(对称差集)
+    集合对象还支持union(并集), intersection(交集), difference(差集)和sysmmetric difference(对称差集)
     等数学运算.
     """
 
 
+def util2():
+    """
+    判断对象的类型:2种方法
+                    isinstance的效率更高
+    :return:
+    """
+    import types
+    tmp_value = 1
+    print isinstance(tmp_value, (types.FloatType, types.IntType))
+
+    if type(tmp_value) in (types.FloatType, types.IntType):
+        print 111
+
+
 if __name__ == '__main__':
-    pass
+    util2()
 
