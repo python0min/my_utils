fd788746f5f283f21e1508f89e40ba18df76e8a4
python0min <pythoner_min@163.com>
2018-11-26 18:23:15 +0800

    1.增加方法

--- pandas_utils/data_analysis_1.py
+++ pandas_utils/data_analysis_1.py
@@ -36,7 +36,7 @@ def set_style(name, height, bold=False):
     return style
 
 
-def foo():
+def func1():
     # 餐别
     DealPeriod = {
         9: u"早餐",
@@ -46,37 +46,50 @@ def foo():
     # read_csv里常用的 names, sep, usecols, dtype, headers, nrows
     # names指文件中没有列名,如果第一行没有列,可以用这个; usecols这个可以用来获取指定列; sep用什么符号分割; nrows : int, default None
     header_user = ['AccNum', 'AccName', 'DepNum']
-    df_1 = pd.read_csv("csv_dir/ac_paymentbooks_his.csv", sep=',', usecols=[2, 3, 4, 6, 9])
-    df_2 = pd.read_csv("csv_dir/id_AccountsInfo.csv", sep=',', names=header_user)
-    df_3 = pd.read_csv("csv_dir/ac_Dealer.csv", sep=',', nrows=10, dtype={'MonDeal': np.int32, 'DealPeriodNo': np.int32})
-    # 对df_1进行筛选
-    df_1 = df_1[(df_1['FeeNum'] == 100) & (df_1['DealPeriodNo'].isin([9, 10, 11]))]
-    print df_1
-    df_1 = df_1['MonDeal'].groupby([df_1['DealerNum'], df_1['StaNum'], df_1['DealPeriodNo']]).agg(['count', 'sum'])
-    # print df_1
+    df_his = pd.read_csv("csv_dir/ac_paymentbooks_his.csv", sep=',', usecols=[2, 3, 4, 6, 9])
+    df_user = pd.read_csv("csv_dir/id_AccountsInfo.csv", sep=',', names=header_user)
+    df_dealer = pd.read_csv("csv_dir/ac_Dealer.csv", sep=',', nrows=10, dtype={'DealerNum': np.int64, 'RecFlag': np.int32})
+    # --------------------data type of columns: df_his.dtypes---------------------
+    # print df_his.dtypes
+    # --------------------对df_dealer的dtype运用-------------------------------------------
+    # print df_dealer
+    # print df_dealer.dtypes
+    # --------------------indexes: df_his.index-----------------------------------
+    # print df_his.index
+    # --------------------columns: df_his.columns-----------------------------------
+    # print df_his.columns
+    # --------------------values: df_his.values-----------------------------------
+    # print df_his.values
+    # --------------------shape: df_his.shape-----------------------------------
+    # print df_his.shape
+    # --------------------对df_his进行where筛选-------------------------------------------
+    df_his = df_his[(df_his['FeeNum'] == 100) & (df_his['DealPeriodNo'].isin([9, 10, 11]))]
+    # print df_his
 
-    # df.loc[df['A'] == 'foo']   df[df['A'] == 'foo']
-    t1 = time.time()
-    # a = df.loc[df['DealerNum'] == 100]
-    t2 = time.time()
-    # print t2 - t1
 
-    # t3 = time.time()
-    # print t3 - t2
-    # df = pd.DataFrame(consume_data)
-    # df_new = df[3].groupby([df[0], df[1], df[2]]).agg(['count', 'sum'])
-    # fos = []
-    # print df_new.unstack()
-    # for d in df_new.to_records():
-    #     fos.append(list(d))
-    # new_df = pd.DataFrame(data=fos, columns=['DealerNum', 'StaNum', 'DealPeriodNo', 'count', 'sum'])
-    # new_df['new_clo'] = new_df['DealerNum'].map(str) + '-' + new_df["StaNum"].map(str)
-    # new_df.drop(columns=['DealerNum', 'StaNum'], inplace=True)
-    #
-    # new_df = new_df.pivot(index='new_clo', columns='DealPeriodNo', values=['count', 'sum']).reset_index().fillna(0)
-    # print new_df
+def func2():
+    """
+    不堆积的2种方法
+    :return:
+    """
+    # 方法一
+    df_his = pd.read_csv("csv_dir/ac_paymentbooks_his.csv", sep=',', usecols=[2, 3, 4, 6, 9])
+    df_his = df_his[(df_his['FeeNum'] == 100) & (df_his['DealPeriodNo'].isin([9, 10, 11]))]
+    df_his = df_his['MonDeal'].groupby([df_his['DealerNum'], df_his['StaNum'], df_his['DealPeriodNo']]).agg(
+        ['count', 'sum'])
 
+    fos = []
+    print df_his.unstack().fillna(0)
+    for d in df_his.to_records():
+        fos.append(list(d))
+    print "------------------------------------------------------------------------------------------------"
+    # 方法二
+    new_df = pd.DataFrame(data=fos, columns=['DealerNum', 'StaNum', 'DealPeriodNo', 'count', 'sum'])
+    new_df['new_clo'] = new_df['DealerNum'].map(str) + '-' + new_df["StaNum"].map(str)
+    new_df.drop(columns=['DealerNum', 'StaNum'], inplace=True)
+    new_df = new_df.pivot(index='new_clo', columns='DealPeriodNo', values=['count', 'sum']).reset_index().fillna(0)
+    print new_df
 
 
 if __name__ == '__main__':
-    foo()
+    func2()
